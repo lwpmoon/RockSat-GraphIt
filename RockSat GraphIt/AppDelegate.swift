debd36500cb4dc7b5fda1039c8fb1ec46586916d
Wesley <lwpmoon@me.com>
2016-07-13 22:04:29 -0600

    Initial Commit

---
+++ RockSat GraphIt/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RockSat GraphIt
+//
+//  Created by Wesley on 7/13/16.
+//  Copyright © 2016 Wesley. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RockSat GraphIt/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RockSat GraphIt
+//
+//  Created by Wesley on 7/13/16.
+//  Copyright © 2016 Wesley. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
